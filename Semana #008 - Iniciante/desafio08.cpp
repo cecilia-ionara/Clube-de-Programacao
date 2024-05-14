6bc7bb1fbb4795c02b0151aa941a58b546cf8e87
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2024-05-14 15:01:38 -0300

    Add files via upload

--- Semana #008 - Iniciante/desafio08.cpp
+++ Semana #008 - Iniciante/desafio08.cpp
@@ -1,15 +1,12 @@
-/* Imprima na tela o valor final do produto a ser
-pago.*/
 
 #include<stdio.h>
+#include <unistd.h>
 
 double preco, preco_final, desconto, juros;
 int opcao;
 
 int main() {
 	do {
-		printf("Qual o preco do produto? ");
-		scanf("%lf", &preco);
 
 		printf("\n\t\t MENU\n");
 		printf("1. A vista em dinheiro ou Pix\n");
@@ -17,34 +14,47 @@ int main() {
 		printf("3. Parcelado em duas vezes\n");
 		printf("4. Parcelado em tres vezes ou mais\n");
 		printf("5. Sair do programa\n\n");
-		
+
 		printf("Qual a forma de pagamento? ");
 		scanf("%d", &opcao);
 
-		//queria colocar um time de uns segundos
-		printf("Ok! Processando...\n");
-		//queria colocar um time de uns segundos
+		if (opcao!=1 && opcao!=2 && opcao!= 3 && opcao!=4 && opcao!=5) {
+			printf("ERRO, favor digitar uma opcao valida! \n");
+			printf("Reiniciando...\n");
+			sleep(1);
+			continue;
+		}
 
+		printf("Qual o preco do produto? ");
+		scanf("%lf", &preco);
+
+		sleep(0.5);
+		printf("Ok! Processando...\n");
+		sleep(2);
 
 		switch(opcao) {
 			case 1:
 				desconto = preco * 0.15;
 				preco_final = preco - desconto;
 				printf("Voce devera pagar R$ %.2lf pelo produto, pois recebeu 15%% de desconto! \n", preco_final);
+				sleep(2);
 				break;
 			case 2:
 				desconto = preco * 0.10;
 				preco_final = preco - desconto;
 				printf("Voce devera pagar R$ %.2lf pelo produto, pois recebeu 10%% de desconto! \n", preco_final);
+				sleep(2);
 				break;
 			case 3:
 				preco_final = preco / 2;
 				printf("Voce devera pagar duas parcelas de R$ %.2lf, sem juros! \n", preco_final);
+				sleep(2);
 				break;
 			case 4:
 				juros = preco * 0.10;
 				preco_final = preco + juros;
 				printf("Voce devera pagar R$ %.2lf pelo produto, com acrescimo de 10%% \n", preco_final);
+				sleep(2);
 				break;
 		}
 		printf("\n");
