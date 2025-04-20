6415fcc7e136de5b5d6ce6582ff0dd2f7efa9f35
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2025-04-20 11:36:30 -0300

    Add files via upload

---
+++ Algortimo/Atividade01/Q2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("Passo a passo de como ganhar os poderes da Gomu Gomu no Mi:\n\n");
+
+    printf("1. Viver na Vila Foosha\n");
+    printf("2. Admirar os piratas desde pequeno\n");
+    printf("3. Conhecer Shanks e sua tripulação\n");
+    printf("4. Querer se tornar pirata\n");
+    printf("5. Frequentar o bar da Makino\n");
+    printf("6. Ver a tripulação do Shanks guardar uma fruta misteriosa\n");
+    printf("7. Encontrar a Gomu Gomu no Mi dentro de um baú\n");
+    printf("8. Achar a fruta estranha e comer por curiosidade\n");
+    printf("9. Ganhar um corpo de borracha após comer a fruta\n");
+    printf("10. Descobrir que não poderá mais nadar\n");
+    printf("11. Ser salvo pelo Shanks e ver ele perdendo um braço pra te salvar\n");
+    printf("12. Prometer se tornar o Rei dos Piratas\n");
+    printf("13. Partir para o mar com o poder da Gomu Gomu no Mi\n\n");
+
+    return 0;
+}
